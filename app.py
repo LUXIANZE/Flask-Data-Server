c75900628eebd83454f2c5292781e8f23f892c13
LUXIANZE <luxianze98@gmail.com>
2021-01-07 06:07:34 +0800

    Simple App with CORS enabled

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import request
+from flask_cors import CORS
+import db
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return 'Server Healthy'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/report', methods=['POST'])
+def report():
+    if request.method == 'POST':
+        payload = request.json
+
+        db.col.insert_one({"mRNNumber":payload['mRNNumber'], "report":payload['report'], "date":payload['date']})
+        return "Success"
---
+++ db.py
@@ -0,0 +1,8 @@
+from flask import Flask
+import pymongo 
+import ssl
+
+CONNECTION_STRING = "mongodb+srv://LU-Developer-98:stJK1QzNedyCQwLL@lu-dev-cluster-5pzdu.mongodb.net/CDSS?retryWrites=true&w=majority"
+client = pymongo.MongoClient(CONNECTION_STRING, ssl_cert_reqs=ssl.CERT_NONE)
+db = client.get_database('CDSS') 
+col = db.reports 
